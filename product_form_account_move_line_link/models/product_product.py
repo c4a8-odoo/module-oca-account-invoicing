8b834ca4bb0c4b9d2f617585be81b4b7762d740c
Jessica Brandl <jessica.brandl@outlook.com>
2025-01-20 09:01:00 +0000

    [MIG] product_form_account_move_line_link: Migration to 18.0

--- product_form_account_move_line_link/models/product_product.py
+++ product_form_account_move_line_link/models/product_product.py
@@ -12,7 +12,7 @@ class ProductProduct(models.Model):
     )
 
     def _compute_account_move_lines_count(self):
-        if not self.user_has_groups("account.group_account_invoice") or not self.ids:
+        if not self.env.user.has_group("account.group_account_invoice") or not self.ids:
             self.purchase_lines_count = 0
             return
         domain = [
